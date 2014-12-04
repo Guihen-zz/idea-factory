31d2c646ef262de9d3c15683e6e887fe48500525
Thiago P Bueno <thiago.pbueno@gmail.com>
2014-12-04 18:08:15 -0200

    Separate my ideas in search results page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,16 @@ class SearchController < ApplicationController
   def idea
   	@query = params[:q]
   	
-  	@ideas = []
+  	@ideas   = []
+  	@my_ideas    = []
+  	@other_ideas = []
+
   	if @query == ""
   		redirect_to :back
   	else
-  		@ideas = Idea.where('name LIKE ? OR description LIKE ?', "%#{@query}%", "%#{@query}%")	
+  		@ideas   = Idea.where('name LIKE ? OR description LIKE ?', "%#{@query}%", "%#{@query}%")
+  		@my_ideas    = @ideas.where(user_id: current_user)
+  		@other_ideas = @ideas - @my_ideas
   	end
   end
 
